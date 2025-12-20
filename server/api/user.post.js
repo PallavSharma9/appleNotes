6165d0cdb25ecbed833654057ef2528cf96b9e42
PallavSharma9 <pallavsharma0202@gmail.com>
2025-12-20 07:27:18 +0530

    Adding hashed password

--- server/api/user.post.js
+++ server/api/user.post.js
@@ -2,13 +2,19 @@
 
 import prisma from "~/server/db/prisma";
 import { readBody, defineEventHandler } from "h3";
+import bcrypt from "bcryptjs";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
+
+  const salt = await bcrypt.genSalt(10);
+  const passwordhash = await bcrypt.hash(body.password, salt);
+
   await prisma.user.create({
     data: {
       email: body.email,
-      password: body.password,
+      password: passwordhash,
+      salt: salt,
     },
   });
   console.log(body);
